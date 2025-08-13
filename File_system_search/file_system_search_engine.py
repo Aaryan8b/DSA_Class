b68ec8ef5e90498ee42a9314bb9127478e471c64
Aaryan8b <aaryan8b@gmail.com>
2025-08-13 15:18:57 +0530

    FileSearchEngine_with_DirectoryMaker

---
+++ File_system_search/file_system_search_engine.py
@@ -0,0 +1,18 @@
+'''
+Problem:
+"File System Search Engine"
+
+Imagine you are building a search tool for a file system (like how Windows Search or find in Linux works).
+
+You have:
+A directory tree (folders can have subfolders and files).
+Each file has a name.
+
+Your task:
+Search for a given file name in the directory tree.
+Use recursion to traverse subfolders (DFS style).
+Use iteration to keep track of files found and their full paths.
+Optimize with a good algorithm so search is fast for large file systems (think pruning unnecessary branches).
+
+'''
+
---
+++ File_system_search/make_folders.py
@@ -0,0 +1,43 @@
+# A program to create multiple folders and sub-folders, and text 2 files at the end
+
+#Main concept:
+'''
+import os
+
+# To create one folder
+
+os.mkdir("my_folder")
+
+# To create nested folders (will create parent directories too)
+
+os.makedirs("parent_folder/child_folder/grandchild_folder", exist_ok=True)
+
+#To make a txt file 
+
+ for k in range(2):
+            file_path = os.path.join(sub_path, f"file_{k}.txt")
+            with open(file_path, "w") as f:
+                f.write(f"This is file {k} inside {sub_path}\n")
+
+'''
+
+#Algorithm
+
+'''
+Will use a for loop to make parent folders in the current directory
+Will use nested loops to make sub folders and files simultaneously
+'''
+
+
+import os
+
+for i in range(3):                              #for folders
+    folder_name="parentfolder"+str(i)
+    os.mkdir(folder_name)
+    for j in range (2):                         #for subfolders
+        subfolder_name="subfolder"+str(j)
+        os.mkdir(subfolder_name)
+        for k in range(2):                      #for text files
+            file_path = f"{folder_name}/{subfolder_name}/file{str(k)}.txt"
+            with open(file_path, "w") as f:
+                f.write(f"This is file {k} inside {folder_name+subfolder_name}\n")
